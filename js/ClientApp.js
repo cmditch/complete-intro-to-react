71d5de556c6118b4d397346a215e2590131f6612
Coury Ditch <cmditch@gmail.com>
2017-02-01 18:22:54 -0700

    Written the buildconfig in webpack and bablrc

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,21 +1,11 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// var React = require('react')
-// var ReactDOM = require('react-dom')
-
 /* global React ReactDOM */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
 
 var div = React.DOM.div
 var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      h1({style: {color: this.props.color} }, this.props.title)
-    )
-  }
-})
-
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
@@ -25,7 +15,8 @@ var MyFirstComponent = React.createClass({
         MyTitleFactory({title: 'Excellent omgz', color: 'peru'}),
         MyTitleFactory({title: 'no more semicolon\'s plz', color: 'mediumaquamarine'}),
         MyTitleFactory({title: 'ur muther', color: 'rebeccapurple'}),
-        MyTitleFactory({title: 'wipe the lint off your shirt', color: 'cornflowerblue'})
+        MyTitleFactory({title: 'wipe the lint off your shirt', color: 'cornflowerblue'}), 
+        <h1>This is a jsx test</h1>
       )
     )
   }
