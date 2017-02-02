08832b201e4f318c0c4e8efb49b31d2fa7685ab2
cmditch <cmditch@gmail.com>
2017-02-02 00:47:51 -0700

    Props yo

--- js/Search.js
+++ js/Search.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import preload from '../public/data.json'
+import ShowCard from './ShowCard'
 
 const Search = React.createClass({
   render () {
     return (
-      <h1>Search page!!</h1>
+      <div className='search'>
+        { preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
+      </div>
     )
   }
 })
