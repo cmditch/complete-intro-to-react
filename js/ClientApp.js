f04680ac0deb075828f40042e921001f185598a4
Coury Ditch <cmditch@gmail.com>
2017-02-01 17:40:31 -0700

    Getting fancier

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,13 +3,15 @@
 // var React = require('react')
 // var ReactDOM = require('react-dom')
 
+/* global React ReactDOM */
+
 var div = React.DOM.div
-var h1  = React.DOM.h1
+var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  render: function() {
+  render: function () {
     return (
-      h1(null, "This isn't really my first compnonent, but any-hoo")
+      h1({style: {color: this.props.color} }, this.props.title)
     )
   }
 })
@@ -17,13 +19,13 @@ var MyTitle = React.createClass({
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-  render: function (){
+  render: function () {
     return (
-      div (null,
-        MyTitleFactory(null),
-        MyTitleFactory(null),
-        MyTitleFactory(null),
-        MyTitleFactory(null)
+      div(null,
+        MyTitleFactory({title: 'Excellent omgz', color: 'peru'}),
+        MyTitleFactory({title: 'no more semicolon\'s plz', color: 'mediumaquamarine'}),
+        MyTitleFactory({title: 'ur muther', color: 'rebeccapurple'}),
+        MyTitleFactory({title: 'wipe the lint off your shirt', color: 'cornflowerblue'})
       )
     )
   }
