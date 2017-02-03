72aee9932b5f7fffd12d49e08c37e80810e5977f
Coury Ditch <cmditch@gmail.com>
2017-02-03 16:21:22 -0700

    Working on specs

--- js/Search.js
+++ js/Search.js
@@ -3,10 +3,38 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+    // This is Bad Practice way to update, see 46 minutes in on Session 4 Video
+    // "You will and should never have to use forceUpdate"
+    // this.state.searchTerm = event.target.value
+    // this.forceUpdate()
+  },
   render () {
     return (
       <div className='search'>
-        { preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        <header>
+          <h1>RouteLog</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          { preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`
+                .toUpperCase().indexOf(this.state.searchTerm
+                .toUpperCase()) >= 0
+            })
+            .map(show => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            })}
+        </div>
       </div>
     )
   }
